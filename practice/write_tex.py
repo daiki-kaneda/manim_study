b3f51390541636b845d0a3ccf5a908d1489d85c7
daiki-kaneda <daiki0622developing@gmail.com>
2025-06-21 15:47:49 +0900

    Add write_tex.py

---
+++ practice/write_tex.py
@@ -0,0 +1,15 @@
+from manim import *
+
+# manim --r 1920,1080 --format=mov --transparent practice/write_tex.py Sceneの名前
+# 上のコマンドでYoutubeのサイズで背景透過された状態でビルドされる
+class WriteTex(Scene):
+    def construct(self):
+        formula = MathTex(r"E = mc^2")
+        self.play(Write(formula))
+        self.wait(1)
+
+class WriteComplexTex(Scene):
+    def construct(self):
+        formula = MathTex(r"\sum{n=1}^{\infty} \frac{1}{n^2} = \frac{\pi^2}{6}")
+        self.play(Write(formula))
+        self.wait(1)
